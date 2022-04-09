ef919d6c0a8428033f9c80dad8c5389ae333494e
ngocbv <ngocbv@remitano.com>
2022-04-09 23:02:40 +0700

    Change folder to store renec installed

--- install/src/defaults.rs
+++ install/src/defaults.rs
@@ -1,21 +1,21 @@
-pub const JSON_RPC_URL: &str = "http://api.devnet.solana.com";
+pub const JSON_RPC_URL: &str = "http://api-testnet.renec.foundation:8899";
 
 lazy_static! {
     pub static ref CONFIG_FILE: Option<String> = {
         dirs_next::home_dir().map(|mut path| {
-            path.extend(&[".config", "solana", "install", "config.yml"]);
+            path.extend(&[".config", "renec", "install", "config.yml"]);
             path.to_str().unwrap().to_string()
         })
     };
     pub static ref USER_KEYPAIR: Option<String> = {
         dirs_next::home_dir().map(|mut path| {
-            path.extend(&[".config", "solana", "id.json"]);
+            path.extend(&[".config", "renec", "id.json"]);
             path.to_str().unwrap().to_string()
         })
     };
     pub static ref DATA_DIR: Option<String> = {
         dirs_next::home_dir().map(|mut path| {
-            path.extend(&[".local", "share", "solana", "install"]);
+            path.extend(&[".local", "share", "renec", "install"]);
             path.to_str().unwrap().to_string()
         })
     };

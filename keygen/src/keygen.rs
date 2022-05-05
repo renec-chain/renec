e42e6c9ab589de62dc03d4253b23e0dc8aad23ab
ngocbv <ngocbv@remitano.com>
2022-05-05 14:44:00 +0700

    Change default renec-keygen output file

--- keygen/src/keygen.rs
+++ keygen/src/keygen.rs
@@ -138,7 +138,7 @@ fn get_keypair_from_matches(
     } else if !config.keypair_path.is_empty() {
         &config.keypair_path
     } else {
-        path.extend([".config", "solana", "id.json"]);
+        path.extend([".config", "renec", "id.json"]);
         path.to_str().unwrap()
     };
     signer_from_path(matches, path, "pubkey recovery", wallet_manager)
@@ -558,7 +558,7 @@ fn do_main(matches: &ArgMatches) -> Result<(), Box<dyn error::Error>> {
             } else if matches.is_present(NO_OUTFILE_ARG.name) {
                 None
             } else {
-                path.extend([".config", "solana", "id.json"]);
+                path.extend([".config", "renec", "id.json"]);
                 Some(path.to_str().unwrap())
             };
 
@@ -601,7 +601,7 @@ fn do_main(matches: &ArgMatches) -> Result<(), Box<dyn error::Error>> {
             let outfile = if matches.is_present("outfile") {
                 matches.value_of("outfile").unwrap()
             } else {
-                path.extend([".config", "solana", "id.json"]);
+                path.extend([".config", "renec", "id.json"]);
                 path.to_str().unwrap()
             };
 

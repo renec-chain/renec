6e3f57f3e211a4045c9d708608565018df9b7316
ngocbv <ngocbv@remitano.com>
2022-06-27 10:13:24 +0700

    Add solana-faucet to build pack

--- sys-tuner/src/lib.rs
+++ sys-tuner/src/lib.rs
@@ -1,11 +1,11 @@
 use log::*;
 
-pub const renec_sys_tuner_PATH: &str = "/tmp/renec-sys-tuner";
+pub const RENEC_SYS_TUNER_PATH: &str = "/tmp/renec-sys-tuner";
 
 #[cfg(unix)]
 pub fn request_realtime_poh() {
     info!("Sending tuning request");
-    let status = unix_socket::UnixStream::connect(renec_sys_tuner_PATH);
+    let status = unix_socket::UnixStream::connect(RENEC_SYS_TUNER_PATH);
     match status {
         Ok(_) => info!("Successfully sent tuning request"),
         Err(err) => warn!(

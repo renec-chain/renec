2ac4d91e8430aa4f03aa549b2deb90d436703d1f
dzungdinh94 <dungnd8594@gmail.com>
2022-07-23 21:27:58 +0700

    [wallet] add staking UI

---
+++ renec-wallet/src/pages/StakingPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import BalancesList from '../components/BalancesList';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core';
+import { useIsExtensionWidth } from '../utils/utils';
+import StakingList from '../components/StakingList';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    [theme.breakpoints.down(theme.ext)]: {
+      padding: 0,
+    },
+    [theme.breakpoints.up(theme.ext)]: {
+      maxWidth: 'md',
+    },
+  },
+  balancesContainer: {
+    [theme.breakpoints.down(theme.ext)]: {
+      marginBottom: 24,
+    },
+  },
+}));
+
+export default function StakingPage() {
+  const classes = useStyles();
+  const isExtensionWidth = useIsExtensionWidth();
+  return (
+    <Container fixed maxWidth="md" className={classes.container}>
+      <Grid container spacing={isExtensionWidth ? 0 : 3}>
+        <Grid md={2} />
+        <Grid item xs={12} md={8} className={classes.balancesContainer}>
+          <StakingList />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}

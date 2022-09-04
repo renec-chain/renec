44e539eadd59c05189645efc9da8447342bd22ad
cuongnguyen11 <cuongnguyen@remitano.com>
2022-09-04 21:54:12 +0700

    [wallet] improve appbar ui on mobile

--- renec-wallet/src/components/DomainsList.tsx
+++ renec-wallet/src/components/DomainsList.tsx
@@ -14,6 +14,7 @@ import {
   TextField,
   FormControlLabel,
   Checkbox,
+  Modal
 } from '@material-ui/core';
 import LoadingIndicator from './LoadingIndicator';
 import SendIcon from '@material-ui/icons/Send';
@@ -21,7 +22,6 @@ import { useUserDomains } from '../utils/name-service';
 import { makeStyles } from '@material-ui/core/styles';
 import { PublicKey, Transaction } from '@solana/web3.js';
 import DnsIcon from '@material-ui/icons/Dns';
-import Modal from '@material-ui/core/Modal';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import GavelIcon from '@material-ui/icons/Gavel';

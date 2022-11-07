731222c5ad9031204ea0d8e29b84cea9ec41ffcb
ngocbv <ngocbv@remitano.com>
2022-11-07 13:28:50 +0700

    chore: config monkier for renec when config url

--- clap-utils/src/input_validators.rs
+++ clap-utils/src/input_validators.rs
@@ -213,9 +213,9 @@ where
 
 pub fn normalize_to_url_if_moniker<T: AsRef<str>>(url_or_moniker: T) -> String {
     match url_or_moniker.as_ref() {
-        "m" | "mainnet-beta" => "https://api.mainnet-beta.solana.com",
-        "t" | "testnet" => "https://api.testnet.solana.com",
-        "d" | "devnet" => "https://api.devnet.solana.com",
+        "m" | "mainnet-beta" => "https://api-mainnet-beta.renec.foundation:8899",
+        "t" | "testnet" => "https://api-testnet.renec.foundation:8899",
+        "d" | "devnet" => "https://api-devnet.renec.foundation:8899",
         "l" | "localhost" => "http://localhost:8899",
         url => url,
     }

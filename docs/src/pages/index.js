7d4f383e0484c06dd254713654d77c3cbbc08cec
Cuong Nguyen Huy <cuongnguyen@remitano.com>
2023-01-27 10:07:07 +0700

    apply logo and renec theme for docs

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -18,6 +18,26 @@ function Home() {
           <div className="container">
             <div className="row cards__container">
               <div className={clsx("col col--4", styles.feature)}>
+                <Link className="navbar__link" to="staking">
+                  <div className="card">
+                    <div className="card__header">
+                      <h3>
+                        <Translate description="run-validator">
+                          Staking
+                        </Translate>
+                      </h3>
+                    </div>
+                    <div className="card__body">
+                      <p>
+                        <Translate description="validate-transactions">
+                          Delegate RENEC to validators and get rewards
+                        </Translate>
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              <div className={clsx("col col--4", styles.feature)}>
                 <Link className="navbar__link" to="running-validator">
                   <div className="card">
                     <div className="card__header">
@@ -32,27 +52,6 @@ function Home() {
                         <Translate description="validate-transactions">
                           Validate transactions, secure the network, and earn
                           rewards.
-                        </Translate>
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-              <div className={clsx("col col--4", styles.feature)}>
-                <Link className="navbar__link" to="rpl-token">
-                  <div className="card">
-                    <div className="card__header">
-                      <h3>
-                        <Translate description="create-spl">
-                          Create an RPL Token
-                        </Translate>
-                      </h3>
-                    </div>
-                    <div className="card__body">
-                      <p>
-                        <Translate description="erc-20">
-                          Launch your own RPL Token, Renec's equivalent of
-                          ERC-20.
                         </Translate>
                       </p>
                     </div>
@@ -80,19 +79,20 @@ function Home() {
                 </Link>
               </div>
               <div className={clsx("col col--4", styles.feature)}>
-                <Link className="navbar__link" to="staking">
+                <Link className="navbar__link" to="rpl-token">
                   <div className="card">
                     <div className="card__header">
                       <h3>
-                        <Translate description="run-validator">
-                          Staking
+                        <Translate description="create-spl">
+                          Create an RPL Token
                         </Translate>
                       </h3>
                     </div>
                     <div className="card__body">
                       <p>
-                        <Translate description="validate-transactions">
-                          Delegate RENEC to validators and get rewards
+                        <Translate description="erc-20">
+                          Launch your own RPL Token, Renec's equivalent of
+                          ERC-20.
                         </Translate>
                       </p>
                     </div>
